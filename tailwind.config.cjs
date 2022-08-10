507e9c43688d50e2b7457e4d34224a2c90ff11d8
fariarahman15 <faria.khanam@northsouth.edu>
2022-08-11 04:15:46 +0600

    Structure done

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,21 +4,19 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        wedgewoodblue: "#45718C",
-        cyanblue: "#77C3F2",
-        tarawera: "#233947",
-        deepblue: "#076395",
-        deepteal: "#016E69",
-        lightteal: "#128984",
+        darkskyblue: "#3797E6",
+        peekablue: "#2B75B3",
+        darkgreyblue: "#375166",
+        dovegrey: "#6A6F73",
+
+        metallicblue: "#45718C",
+        carolinablue: "#7EB9EA",
+        regalblue: "#184366",
+
+        darkslategrey: "#233947",
+        vistawhite: "#FAFAFA",
         plainwhite: "#FFFFFF",
         plainblack: "#000000",
-        deepyellow: "#FFCC1D",
-        hoveryellow: "#FAD34F",
-        lightblue: "#22A9E0",
-        deepgray: "#707070",
-        lightgray: "#AAA9A9",
-        ravenblack: "#3C3C3C",
-        greenblue: "#1068B4",
       },
     },
   },
