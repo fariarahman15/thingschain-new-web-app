9787aa019e965411d94e1c88e6a45bc5c4d1fb8d
fariarahman15 <faria.khanam@northsouth.edu>
2022-08-10 19:45:50 +0600

    update

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,25 @@
 module.exports = {
   content: ["./index.html"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        wedgewoodblue: "#45718C",
+        cyanblue: "#77C3F2",
+        tarawera: "#233947",
+        deepblue: "#076395",
+        deepteal: "#016E69",
+        lightteal: "#128984",
+        plainwhite: "#FFFFFF",
+        plainblack: "#000000",
+        deepyellow: "#FFCC1D",
+        hoveryellow: "#FAD34F",
+        lightblue: "#22A9E0",
+        deepgray: "#707070",
+        lightgray: "#AAA9A9",
+        ravenblack: "#3C3C3C",
+        greenblue: "#1068B4",
+      },
+    },
   },
   plugins: [],
 };
